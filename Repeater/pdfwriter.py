6c82ed87a84da687b1e5a2cd4611213b2aec648b
KeikeiSo <calliasu2018@gmail.com>
2021-05-29 11:47:08 -0400

    Create pdfwriter.py

---
+++ Repeater/pdfwriter.py
@@ -0,0 +1,25 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+
+packet = io.BytesIO()
+# create a new PDF with Reportlab
+can = canvas.Canvas(packet, pagesize=letter)
+can.drawString(10, 100, "Hello world")
+can.save()
+
+#move to the beginning of the StringIO buffer
+packet.seek(0)
+new_pdf = PdfFileReader(packet)
+# read your existing PDF
+existing_pdf = PdfFileReader(open("C:/Users/calli/OneDrive/Desktop/Past course/MATH 235/aata-20170805.pdf", "rb"))
+output = PdfFileWriter()
+# add the "watermark" (which is the new pdf) on the existing page
+page = existing_pdf.getPage(0)
+page.mergePage(new_pdf.getPage(0))
+output.addPage(page)
+# finally, write "output" to a real file
+outputStream = open("destination.pdf", "wb")
+output.write(outputStream)
+outputStream.close()
